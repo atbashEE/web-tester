a26f2c4708c09e12dfc9c864b6aa50987dc379aa
Rudy De Busscher <rdebusscher@gmail.com>
2018-04-02 19:39:14 +0200

    Fix PublicAPI retention

--- code/src/main/java/be/atbash/ee/test/ui/PublicAPI.java
+++ code/src/main/java/be/atbash/ee/test/ui/PublicAPI.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2017 Rudy De Busscher
+ * Copyright 2017-2018 Rudy De Busscher
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -23,6 +23,6 @@ import java.lang.annotation.*;
  */
 @Documented
 @Target({ElementType.TYPE})
-@Retention(RetentionPolicy.RUNTIME)
+@Retention(RetentionPolicy.SOURCE)
 public @interface PublicAPI {
 }
